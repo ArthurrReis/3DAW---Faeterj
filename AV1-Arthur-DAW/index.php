c1f14fe15743af99628bf5666e43a3646e1bb4ce
Arthur Reis <arthurcereis2005@gmail.com>
2026-05-31 17:57:20 -0300

    Requisições em Js

--- AV1-Arthur-DAW/index.php
+++ AV1-Arthur-DAW/index.php
@@ -1,8 +1,20 @@
-<h1>Questionario</h1>
-<ul>
-    <li><a href="criarusuario.php">Cadastrar Usuário</a></li>
-    <li><a href="criarperguntatexto.php">Criar Pergunta de Texto</a></li>
-    <li><a href="criarperguntamultipla.php">Criar Pergunta de Múltipla Escolha</a></li>
-    <li><a href="listarperguntas.php">Listar Todas as Perguntas</a></li>
-    <li><a href="alterarpergunta.php">Alterar/Excluir Perguntas</a></li>
-</ul>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <link rel="stylesheet" href="../_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title>Menu Principal - Questionario</title>
+</head>
+<body>
+    <div>
+        <h1>Questionario</h1>
+        <ul>
+            <li><a href="criarusuario.php">Cadastrar Usuário</a></li>
+            <li><a href="criarperguntatexto.php">Criar Pergunta de Texto</a></li>
+            <li><a href="criarperguntamultipla.php">Criar Pergunta de Múltipla Escolha</a></li>
+            <li><a href="listarperguntas.php">Listar Todas as Perguntas</a></li>
+            <li><a href="alterarpergunta.php">Alterar/Excluir Perguntas</a></li>
+        </ul>
+    </div>
+</body>
+</html>
